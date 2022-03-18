97396d52d040f88726e23e2cbce20dcbfb97d0c6
Prajwal-Bellignur <prajwal.bellignur@gmail.com>
2022-03-18 00:58:21 -0700

    testing

--- App.js
+++ App.js
@@ -4,8 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text style={styles.testing}>Hello World! This is my first attempt at making React native apps from scratch!</Text>
-      <StatusBar style="auto" />
+
+    {/** tasks for today */}
+    <View style={styles.taskWrapper}> 
+                          
+    </View>
+
     </View>
   );
 }
@@ -14,13 +18,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  testing: {
-    padding: 10,
-    fontSize: 15,
-    color: 'darkgreen',
-    backgroundColor: 'lightyellow',
   }
 });
