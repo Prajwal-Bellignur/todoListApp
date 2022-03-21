7d156c44c7452eb4747a23d3d72133a9ecd6ec45
Prajwal-Bellignur <prajwal.bellignur@gmail.com>
2022-03-21 00:50:38 -0700

    Implemented the add button and text field. Need to fix keyboard enter function.

--- App.js
+++ App.js
@@ -1,8 +1,38 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react'
+import { KeyboardAvoidingView, StyleSheet, Text, View, TextInput, TouchableOpacity, Keyboard, Alert } from 'react-native';
 import Task from './components/Task.js';
 
 export default function App() {
+
+  const [task, setTask] = useState();
+  const [taskItems, setTaskItems] = useState([]);
+
+
+  const handleAddTask = () => {
+    if(task != null) {
+      Keyboard.dismiss();
+      setTaskItems([...taskItems, task]);
+      setTask(null);
+    } else{
+      Alert.alert("Invalid Entry", "Please enter a task", "OK");
+    }
+  }
+
+  const completeTask = (index) => {
+    let itemsCopy =[...taskItems];
+    itemsCopy.splice(index, 1);
+    setTaskItems(itemsCopy);
+  }
+
+  // to add a task if the enter key is pressed -> not working
+  const handleKeyDown = () => {
+    console.log("here");
+    if(e.key == "Enter") {
+      handleAddTask();
+    }
+  }
+
   return (
     <View style={styles.container}>
 
@@ -13,12 +43,31 @@ export default function App() {
         <View style={styles.items}>
           {/* this is where the tasks will go */}
 
-          <Task text={'Task 1'}/>
-          <Task text={'Task 2'}/>
+          {
+            taskItems.map((item, index) => {
+              return (
+                <TouchableOpacity key={index} onPress={() => completeTask(index)}>
+                  <Task text={item}/>
+                </TouchableOpacity>
+              )
+            })
+          }
 
         </View>
 
     </View>
+
+    {/* Write task */}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === "ios" ? "padding" : "height"}
+      style={styles.writeTaskWrapper}>
+        <TextInput style={styles.input} placeholder={'Write a task!'} value={task} onChangeText={text => setTask(text)} onKeyDown={(e) => handleKeyDown(e)}/>
+        <TouchableOpacity onPress={() => handleAddTask()}>
+          <View style={styles.AddWrapper}>
+            <Text style={styles.addText}>+</Text>
+          </View>
+        </TouchableOpacity>
+      </KeyboardAvoidingView>
 
     </View>
   );
@@ -39,6 +88,36 @@ const styles = StyleSheet.create({
   },
   items: {
     marginTop: 30,
+  },
+  writeTaskWrapper: {
+    position: 'absolute',
+    bottom: 60,
+    width: '100%',
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center',
+  },
+  input: {
+    paddingVertical: 15,
+    paddingHorizontal: 15,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+    width: 250,
+  },
+  AddWrapper: {
+    width: 60,
+    height: 60,
+    backgroundColor: '#FFF',
+    borderRadius: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#C0C0C0',
+    borderWidth: 1,
+  },
+  addText: {
+
   },
 
 });
